cd9aff8a34ba1e6eae454c8dad0bed3c673c8b95
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-03-17 07:36:19 +0300

    dom modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code here!
+// Remove the <main> element with id "main"
+document.querySelector('main#main').remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id of the new <h1> to "victory"
+newHeader.id = 'victory';
+
+// Set the text content of newHeader
+newHeader.textContent = 'Sheilah is the champion'; 
+
+// Append the new <h1> element to the body
+document.body.appendChild(newHeader);
